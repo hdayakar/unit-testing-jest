66005369bdd785436ef8eb00c85a05f3a101e903
Dayakar <hdayakar@gmail.com>
2018-09-25 14:00:02 +0530

    add async call using axios

--- functions.test.js
+++ functions.test.js
@@ -42,3 +42,11 @@ test('Admin shoud be in usernames', () => {
     usernames = ['john', 'karen', 'admin'];
     expect(usernames).toContain('admin');
 });
+
+// working with async data
+test('User fetched name should be Leanne Graham', () => {
+    expect.assertions(1);
+    return functions.fetchUser().then(data => {
+        expect(data.name).toEqual('Leanne Graham');
+    })
+})
