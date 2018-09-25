88d00b1656c02edb7dd8c4543814d8c851e057b1
Dayakar <hdayakar@gmail.com>
2018-09-25 11:03:22 +0530

    test not

--- functions.test.js
+++ functions.test.js
@@ -2,4 +2,14 @@ const functions = require('./functions');
 
 test('Adds 2 + 2 to equal 4', () => {
     expect(functions.add(2,2)).toBe(4);
-})
+});
+
+test('Adds 2 + 2 to NOT equal 5', () => {
+    expect(functions.add(2, 2)).not.toBe(5);
+});
+
+// toBeNull matches only null
+// toBeUndefined matches only undefined
+// toBeDefined is the opposite of toBeUndefined
+// toBeTruthy matches anything that an if statement treats as true
+// toBeFalsy matches anything that an if statement treats as false
